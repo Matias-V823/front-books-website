3727098ca13af1d07c20b7e257026ed27c4a0f75
Matias-V823 <matias.varas62@gmail.com>
2025-06-23 14:44:13 -0400

    implementé el filtro y busqueda por categoría en Home

--- src/Components/CardBook.tsx
+++ src/Components/CardBook.tsx
@@ -1,34 +1,36 @@
-import { useAppStore } from "../store/useAppStore";
+import type { bookType } from "../types";
 
-const CardBook = () => {
-    const books = useAppStore((state) => state.books);
+type cardBookType = {
+    books: bookType[]
+}
 
-    if (!Array.isArray(books)) return <p>No hay libros disponibles.</p>;
+const CardBook = ({ books } : cardBookType) => {
+  if (!Array.isArray(books) || books.length === 0) return <p>No hay libros disponibles.</p>;
 
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {books.map((book) => (
-                <div key={book.idBook} className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-                    <img
-                        className="w-full h-48 object-cover"
-                        src={
-                            book.image64
-                                ? `data:image/jpeg;base64,${book.image64}`
-                                : "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
-                        }
-                        alt={book.title}
-                    />
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">{book.title}</div>
-                        <p className="text-gray-700 text-base mb-1">{book.author}</p>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-                            {book.type}
-                        </span>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {books.map((book) => (
+        <div key={book.idBook} className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+          <img
+            className="w-full h-48 object-cover"
+            src={
+              book.image64
+                ? `data:image/jpeg;base64,${book.image64}`
+                : "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
+            }
+            alt={book.title}
+          />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{book.title}</div>
+            <p className="text-gray-700 text-base mb-1">{book.author}</p>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+              {book.type}
+            </span>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default CardBook;
--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,14 +1,38 @@
-import { useEffect } from "react"
-import CardBook from "../Components/CardBook"
-import { useAppStore } from "../store/useAppStore"
+import { useEffect, useState } from "react";
+import CardBook from "../Components/CardBook";
+import { useAppStore } from "../store/useAppStore";
+import type { bookType } from "../types"; 
 
 const Home = () => {
   const getBooks = useAppStore((state) => state.getBooks);
-  const categories = ['Ficción', 'No Ficción', 'Ciencia', 'Bibliografía']
+  const allBooks = useAppStore((state) => state.books); 
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("Todos");
+  const [filteredBooks, setFilteredBooks] = useState<bookType[]>([]);
+
 
   useEffect(() => {
-    getBooks()
-  }, [])
+    getBooks();
+  }, []);
+
+  useEffect(() => {
+    handleSearchBook();
+  }, [allBooks]);
+
+  const handleSearchBook = () => {
+    const term = searchTerm.toLowerCase();
+
+    const result = allBooks.filter(book => {
+      const matchTitle = book.title.toLowerCase().includes(term);
+      const matchAuthor = book.author.toLowerCase().includes(term);
+      const matchCategory = selectedCategory === "Todos" || book.type === selectedCategory;
+
+      return (matchTitle || matchAuthor) && matchCategory;
+    });
+
+    setFilteredBooks(result);
+  };
 
   return (
     <div className="p-4">
@@ -17,32 +41,43 @@ const Home = () => {
           Buscar libro
         </label>
         <div className="flex gap-2 bg-gradient-to-r from-blue-600 to-blue-800 p-4 rounded-lg">
-          <select className="px-3 py-2 text-white border border-gray-300 rounded-md  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+          <select
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+            className="px-3 py-2 text-white border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          >
             <option className="text-zinc-900">Todos</option>
-            {categories.map((category, index) => (
-              <option key={index} className="text-zinc-900">{category}</option>
+            {allBooks.map((category, index) => (
+              <option key={index} className="text-zinc-900">
+                {category.type}
+              </option>
             ))}
           </select>
+
           <input
             type="text"
             id="search"
-            className="flex-1 px-3 py-2 text-white  border border-gray-100 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="flex-1 px-3 py-2 text-white border border-gray-100 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             placeholder="Título o autor..."
           />
-          <button className="bg-white text-blue-600 hover:bg-blue-50 font-medium py-2 px-4 rounded-full transition-all duration-300 shadow-md">
+
+          <button
+            onClick={handleSearchBook}
+            className="bg-white text-blue-600 hover:bg-blue-50 font-medium py-2 px-4 rounded-full transition-all duration-300 shadow-md"
+          >
             Buscar
           </button>
-
-
         </div>
       </div>
 
       <section>
         <h1 className="text-2xl font-bold mb-6">Explora nuestra colección</h1>
-        <CardBook/>
+        <CardBook books={filteredBooks.length ? filteredBooks : allBooks} />
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
