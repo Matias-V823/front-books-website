2644a0e12af043950e35c7a4484ff395550e86cf
Matias-V823 <matias.varas62@gmail.com>
2025-06-22 20:09:26 -0400

    creé los types de las tablas de la BD para manejar las peticiones http

---
+++ src/types/index.ts
@@ -0,0 +1,49 @@
+export type bookType = {
+  id: number;
+  author: string;
+  image: string | null;
+  title: string;
+  type: string;
+};
+
+export type bookingType = {
+  id_booking: number;
+  date_booking: string;
+  date_return: string;
+  state: boolean;
+  copybook: number;
+  user_fk: string;
+};
+
+export type copyBookType = {
+  id_copybook: number;
+  state: boolean;
+  book: number;
+};
+
+export type fineType = {
+  id_fine: number;
+  amount: number;
+  description: string;
+  state: boolean;
+  user_fk: string;
+};
+
+export type rolType = {
+  id: number;
+  name: string;
+};
+
+export type userLibraryType = {
+  email: string;
+  last_name: string;
+  name: string;
+  password: string;
+  state: boolean;
+};
+
+export type userRole = {
+  id_rol_user: number;
+  id_rol: number;
+  email: string;
+};
